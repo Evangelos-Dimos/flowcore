90cca5d3c9f358c792d54972513f6c02fd287d95
Christos Thanatis <christosthanatis@gmail.com>
2026-04-19 19:13:55 +0300

    Fix Check_Inventory

--- scripts/check_inventory.sh
+++ scripts/check_inventory.sh
@@ -1,22 +1,11 @@
 INVENTORY_FILE="../data/inventory.txt"
 LOG_FILE="../logs/system.log"
 
-<<<<<<< HEAD
-INVENTORY_FILE="../data/inventory.txt"
-LOG_FILE="../logs/system.log"
-
-#Checks if the file exists
-if [ ! -f "../data/inventory.txt" ]; then
-        echo "[ERROR] Inventory file not found" >> ../logs/system.log
-        echo "Inventory file not found"
-        exit 1
-=======
 # file exists?
 if [ ! -f "$INVENTORY_FILE" ]; then
     echo "Inventory file not found"
     echo "[ERROR] Inventory file missing" >> "$LOG_FILE"
     exit 1
->>>>>>> 6f62d1f (Changes for receive, assign, check)
 fi
 
 # empty?
@@ -25,24 +14,6 @@ if [ ! -s "$INVENTORY_FILE" ]; then
     exit 0
 fi
 
-<<<<<<< HEAD
-echo "FlowCore Warehouse - Inventory Check"
-
-#Prints all products
-echo "ID,Type,Quantity,Location"
-
-while IFS=',' read -r id type quantity location; 
-	do
-        	echo "$id,$type,$quantity,$location"
-	done < "$INVENTORY_FILE"
-
-total=$(wc -l < "$INVENTORY_FILE")
-echo "Total unique items: $total"
-
-#Logs completion
-echo "[INFO] Inventory check completed. Total items: $total" >> "$LOG_FILE"
-=======
 echo "------ INVENTORY ------"
 cat "$INVENTORY_FILE"
 echo "-----------------------"
->>>>>>> 6f62d1f (Changes for receive, assign, check)
